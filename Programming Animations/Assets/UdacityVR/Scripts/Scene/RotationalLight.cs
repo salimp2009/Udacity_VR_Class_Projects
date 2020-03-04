efa3b29f766bb20e42d1907a634a3b2fae7afc5f
salimp2009 <salimp2009@gmail.com>
2020-03-04 15:46:21 +0300

    addingVR_Interaction/Take_A_Vacation; script did not work to change the scene, Adding Programming Animation:Script for rotating the sun light thru Script; works Ok

---
+++ Programming Animations/Assets/UdacityVR/Scripts/Scene/RotationalLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationalLight : MonoBehaviour {
+	public GameObject directionalLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		directionalLight.transform.Rotate(0.0f, Time.deltaTime, 0.0f);
+	}
+}
---
+++ VR-Software-Development_VR-Interaction-2.0.0/VR Interaction/Assets/changeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+    
+
+    public void GoToScene(string sceneName) {
+        Debug.Log("My method is called" );
+        SceneManager.LoadScene(sceneName);
+    }
+}
